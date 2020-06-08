408d08c9432166570c38bfd9a07196e9baf44dc3
邓伟 <dw.greyman@gmail.com>
2020-06-08 14:20:45 -0400

    .

--- utils/plot_criteo_R_matrix.py
+++ utils/plot_criteo_R_matrix.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# change the directory to your settings
+
 flights = pd.read_csv('//Users//wayne//Desktop//criteo_R.csv', header=None)
 
 flights.index += 1 
@@ -16,7 +16,4 @@ plt.figure(figsize=(8, 7))
 #sns.heatmap(flights, vmin=-1.1, vmax=1.1, linewidths=.0005, cmap=sns.color_palette("BrBG", 100), xticklabels=7, yticklabels=7)
 sns.heatmap(flights, vmin=-1.1, vmax=1.1, linewidths=.0005, cmap=sns.color_palette("BrBG", 100), xticklabels=False, yticklabels=False)
 
-
-#sns_plot.savefig("criteo_r.png")
-
-plt.savefig('//Users//wayne//Desktop//criteo_r.png', figsize=(8, 7))
+plt.savefig('//Users//wayne//Desktop//criteo_r.png', figsize=(8, 7), bbox_inches='tight')
