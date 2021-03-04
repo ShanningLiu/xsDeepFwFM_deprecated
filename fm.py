950b5dfaa3ed0213ce92d06495a0fca9b2b40d9e
Andreas Peintner <a.peintner@gmx.net>
2021-03-04 15:37:29 +0100

    quantization

--- fm.py
+++ fm.py
@@ -74,20 +74,21 @@ def inference_time(model, data_loader, device):
     with torch.no_grad():
         for i, (fields, target) in enumerate(tqdm.tqdm(data_loader, smoothing=0, mininterval=1.0)):
             fields, target = fields.to(device), target.to(device)
-            if i < 100:  # warmup
+            if len(data_loader) < i < 100:  # warmup
                 y = model(fields)
             else:
-                with torch.autograd.profiler.profile(use_cuda=False) as prof:
+                with torch.autograd.profiler.profile(with_stack=True, use_cuda=False) as prof:
                     y = model(fields)
 
-        print(prof.key_averages().table(sort_by="self_cpu_time_total"))
+        print(prof.key_averages().table(sort_by='self_cpu_time_total', row_limit=10))
+        #print(prof.key_averages(group_by_stack_n=5).table(sort_by='self_cpu_time_total', row_limit=20))
 
 
 if __name__ == '__main__':
     batch_size = 2048
     epochs = 1
-    #dataset = CriteoDataset("G://dac//train_ssss.txt")
-    dataset = TwitterDataset("G://dac//twitter_final_s.parquet", twitter_label='like')
+    dataset = CriteoDataset("G://dac//train_ssss.txt")
+    #dataset = TwitterDataset("G://dac//twitter_final_s.parquet", twitter_label='like')
     train_length = int(len(dataset) * 0.8)
     valid_length = int(len(dataset) * 0.1)
     test_length = len(dataset) - train_length - valid_length
@@ -97,17 +98,17 @@ if __name__ == '__main__':
     valid_data_loader = DataLoader(valid_dataset, batch_size=batch_size, num_workers=0)
     test_data_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=0)
 
-    #device = torch.device('cuda:0')
-    #torch.cuda.empty_cache()
-    device = torch.device('cpu')
+    device = torch.device('cuda:0')
+    torch.cuda.empty_cache()
+    #device = torch.device('cpu')
 
     field_dims = dataset.field_dims
 
     #model = DeepFactorizationMachineModel(field_dims=field_dims, embed_dim=10, mlp_dims=(16, 16), dropout=0.2).to(device)
-    #model = DeepFieldWeightedFactorizationMachineModel(field_dims=field_dims, embed_dim=10, use_fwlw=True, use_lw=False, mlp_dims=(400, 400, 400), dropout=0.2).to(device)
+    #model = DeepFieldWeightedFactorizationMachineModel(field_dims=field_dims, embed_dim=10, use_fwlw=False, use_lw=False, use_emb_bag=False, use_qr_emb=False, mlp_dims=(400, 400, 400), dropout=0.2, batch_norm=False).to(device)
 
-    model = FactorizationMachineModel(field_dims=field_dims, embed_dim=10).to(device)
-    #model = FieldWeightedFactorizationMachineModel(field_dims=field_dims, embed_dim=10, use_fwlw=True, use_lw=False).to(device)
+    #model = FactorizationMachineModel(field_dims=field_dims, embed_dim=10, use_lw=False).to(device)
+    model = FieldWeightedFactorizationMachineModel(field_dims=field_dims, embed_dim=10, use_fwlw=False, use_lw=False, use_emb_bag=False, use_qr_emb=False).to(device)
 
     criterion = torch.nn.BCELoss()
     optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001, weight_decay=1e-6)
@@ -121,6 +122,42 @@ if __name__ == '__main__':
     loss, auc, prauc, rce = test(model, test_data_loader, device)
     print(f'test loss: {loss:.6f} auc: {auc:.6f} prauc: {prauc:.4f} rce: {rce:.4f}')
 
-    '''mini_dataset, _ = torch.utils.data.random_split(dataset, (300, len(dataset) - 300))
+    mini_dataset, _ = torch.utils.data.random_split(dataset, (300, len(dataset) - 300))
+    mini_data_loader = DataLoader(mini_dataset, batch_size=1, num_workers=0)
+    inference_time(model, mini_data_loader, torch.device('cpu'))
+
+    '''
+    quantized_model = model.eval()
+    device = torch.device('cpu')
+    quantized_model.to(device)
+
+    quantized_model.qconfig = torch.quantization.default_qconfig#torch.quantization.get_default_qconfig('fbgemm')
+    quantized_model.fwfm.embeddings.qconfig = None#torch.quantization.float_qparams_weight_only_qconfig
+    quantized_model.linear.qconfig = None
+    quantized_model.fwfm.field_cov.qconfig = None
+    quantized_model.fwfm_linear.qconfig = None
+
+    # TODO cannot fuse linear and relu because batch norm in between
+    # https: // github.com / PyTorchLightning / pytorch - lightning / issues / 2544
+    # https://discuss.pytorch.org/t/batch-normalization-of-linear-layers/20989/9 where to put batchnorm
+    #quantized_model = torch.quantization.fuse_modules(quantized_model,
+                                                      #[['mlp.mlp.0', 'mlp.mlp.1', 'mlp.mlp.2'],
+                                                      # ['mlp.mlp.4', 'mlp.mlp.5', 'mlp.mlp.6'],
+                                                      # ['mlp.mlp.8', 'mlp.mlp.9', 'mlp.mlp.10']])
+    quantized_model = torch.quantization.fuse_modules(quantized_model,
+                                                          [['mlp.mlp.0', 'mlp.mlp.1'],
+                                                           ['mlp.mlp.3', 'mlp.mlp.4'],
+                                                           ['mlp.mlp.6', 'mlp.mlp.7']])
+
+    torch.quantization.prepare(quantized_model, inplace=True)
+    _, _, _, _ = test(quantized_model, test_data_loader, device)
+
+    torch.quantization.convert(quantized_model, inplace=True)
+
+    quantized_model.mlp.quantize = True
+    loss, auc, prauc, rce = test(quantized_model, test_data_loader, device)
+    print(f'test loss: {loss:.6f} auc: {auc:.6f} prauc: {prauc:.4f} rce: {rce:.4f}')
+
+    mini_dataset, _ = torch.utils.data.random_split(dataset, (300, len(dataset) - 300))
     mini_data_loader = DataLoader(mini_dataset, batch_size=1, num_workers=0)
     inference_time(model, mini_data_loader, torch.device('cpu'))'''
--- model/FM.py
+++ model/FM.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from model.Datasets import Dataset, get_dataset
+from model.QREmbeddingBag import QREmbeddingBag
 
 
 class FeaturesLinear(torch.nn.Module):
@@ -25,37 +26,43 @@ class FeaturesEmbedding(torch.nn.Module):
     def __init__(self, field_dims, embed_dim):
         super().__init__()
         self.embedding = torch.nn.Embedding(sum(field_dims), embed_dim)
-        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
         torch.nn.init.xavier_uniform_(self.embedding.weight.data)
 
     def forward(self, x):
         """
         :param x: Long tensor of size ``(batch_size, num_fields)``
         """
-        x = x + x.new_tensor(self.offsets).unsqueeze(0)
         return self.embedding(x)
 
 
 class MultiLayerPerceptron(torch.nn.Module):
 
-    def __init__(self, input_dim, embed_dims, dropout, output_layer=True):
+    def __init__(self, input_dim, embed_dims, dropout, output_layer=True, quantize=False, batch_norm=False):
         super().__init__()
         layers = list()
         for embed_dim in embed_dims:
             layers.append(torch.nn.Linear(input_dim, embed_dim))
-            layers.append(torch.nn.BatchNorm1d(embed_dim))
+            if batch_norm:
+                layers.append(torch.nn.BatchNorm1d(embed_dim))
             layers.append(torch.nn.ReLU())
             layers.append(torch.nn.Dropout(p=dropout))
             input_dim = embed_dim
         if output_layer:
             layers.append(torch.nn.Linear(input_dim, 1))
         self.mlp = torch.nn.Sequential(*layers)
+        self.quantize = quantize
+        self.quant = torch.quantization.QuantStub()
+        self.dequant = torch.quantization.DeQuantStub()
 
     def forward(self, x):
         """
         :param x: Float tensor of size ``(batch_size, embed_dim)``
         """
-        return self.mlp(x)
+        if self.quantize:
+            x = self.quant(x)
+            return self.dequant(self.mlp(x))
+        else:
+            return self.mlp(x)
 
 
 class DeepFactorizationMachineModel(torch.nn.Module):
@@ -88,32 +95,35 @@ class DeepFactorizationMachineModel(torch.nn.Module):
 
 
 class DeepFieldWeightedFactorizationMachineModel(torch.nn.Module):
-    def __init__(self, field_dims, embed_dim, mlp_dims, dropout, use_lw=False, use_fwlw=False):
+    def __init__(self, field_dims, embed_dim, mlp_dims, dropout, use_lw=False, use_fwlw=False, use_emb_bag=False, use_qr_emb=False, quantize_dnn=False, batch_norm=False):
         super().__init__()
         self.num_fields = len(field_dims)
         self.use_lw = use_lw
         self.use_fwlw = use_fwlw
+        self.use_emb_bag = use_emb_bag
+        self.use_qr_emb = use_qr_emb
         self.linear = FeaturesLinear(field_dims)
         self.fwfm_linear = torch.nn.Linear(embed_dim, self.num_fields, bias=False)
-        self.fwfm = FieldWeightedFactorizationMachine(field_dims, embed_dim)
+        self.fwfm = FieldWeightedFactorizationMachine(field_dims, embed_dim, use_emb_bag=use_emb_bag, use_qr_emb=use_qr_emb)
         self.embed_output_dim = len(field_dims) * embed_dim
-        self.mlp = MultiLayerPerceptron(self.embed_output_dim, mlp_dims, dropout)
+        self.mlp = MultiLayerPerceptron(self.embed_output_dim, mlp_dims, dropout, quantize=quantize_dnn, batch_norm=batch_norm)
 
     def forward(self, x):
         """
         :param x: Long tensor of size ``(batch_size, num_fields)``
         """
-        x_offset = x + x.new_tensor(self.fwfm.offsets).unsqueeze(0)
-        embed_x = [self.fwfm.embeddings[i](x_offset[:, i]) for i in range(self.num_fields)]
-
-        fwfm_linear = torch.einsum('ijk,ik->ijk', [torch.stack(embed_x), self.fwfm_linear.weight])
-        fwfm_first_order = torch.sum(torch.einsum('ijk->ji', [fwfm_linear]), dim=1, keepdim=True)
+        if self.use_emb_bag or self.use_qr_emb:
+            embed_x = [self.fwfm.embeddings[i](torch.unsqueeze(x[:, i], 1)) for i in range(self.num_fields)]
+        else:
+            embed_x = [self.fwfm.embeddings[i](x[:, i]) for i in range(self.num_fields)]
 
         fwfm_second_order = torch.sum(self.fwfm(torch.stack(embed_x)), dim=1, keepdim=True)
 
         if self.use_lw:
             x = self.linear(x) + fwfm_second_order + self.mlp(torch.cat(embed_x, 1))
         elif self.use_fwlw:
+            fwfm_linear = torch.einsum('ijk,ik->ijk', [torch.stack(embed_x), self.fwfm_linear.weight])
+            fwfm_first_order = torch.sum(torch.einsum('ijk->ji', [fwfm_linear]), dim=1, keepdim=True)
             x = fwfm_first_order + fwfm_second_order + self.mlp(torch.cat(embed_x, 1))
         else:
             x = fwfm_second_order + self.mlp(torch.cat(embed_x, 1))
@@ -123,22 +133,37 @@ class DeepFieldWeightedFactorizationMachineModel(torch.nn.Module):
 
 class FieldWeightedFactorizationMachine(torch.nn.Module):
 
-    def __init__(self, field_dims, embed_dim):
+    def __init__(self, field_dims, embed_dim, use_emb_bag=False, use_qr_emb=False):
         super().__init__()
         self.num_fields = len(field_dims)
-
         self.embed_dim = embed_dim
-        self.embeddings = torch.nn.ModuleList([
-            torch.nn.Embedding(sum(field_dims), embed_dim) for _ in range(self.num_fields)
-        ])
-        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
-        for embedding in self.embeddings:
-            torch.nn.init.xavier_uniform_(embedding.weight.data)
-
+        if use_emb_bag or use_qr_emb:
+            self.embeddings = self.create_emb(embed_dim, field_dims, use_qr_emb)
+        else:
+            self.embeddings = torch.nn.ModuleList([
+                torch.nn.Embedding(field_dim, embed_dim) for field_dim in field_dims
+            ])
+            for embedding in self.embeddings:
+                torch.nn.init.xavier_uniform_(embedding.weight.data)
         self.field_cov = torch.nn.Linear(self.num_fields, self.num_fields, bias=False)
 
+    def create_emb(self, m, ln, qr=False, qr_operation="mult", qr_collisions=1, qr_threshold=200):
+        emb_l = torch.nn.ModuleList()
+        for i in range(0, ln.size):
+            n = ln[i]
+            # construct embedding operator
+            if qr and n > qr_threshold:
+                EE = QREmbeddingBag(n, m, qr_collisions,
+                                    operation=qr_operation, mode="sum", sparse=False)
+            else:
+                EE = torch.nn.EmbeddingBag(n, m, mode="sum", sparse=False)
+                torch.nn.init.xavier_uniform_(EE.weight.data)
+
+            emb_l.append(EE)
+
+        return emb_l
+
     def forward(self, x):
-        # compute outer product, outer_fm: 39x39x2048x10
         outer_fm = torch.einsum('kij,lij->klij', x, x)
         outer_fwfm = torch.einsum('klij,kl->klij', outer_fm, (self.field_cov.weight.t() + self.field_cov.weight) * 0.5)
         fwfm_second_order = (torch.sum(torch.sum(outer_fwfm, 0), 0) - torch.sum(torch.einsum('kkij->kij', outer_fwfm), 0)) * 0.5
@@ -148,36 +173,39 @@ class FieldWeightedFactorizationMachine(torch.nn.Module):
 
 class FieldWeightedFactorizationMachineModel(torch.nn.Module):
 
-    def __init__(self, field_dims, embed_dim, use_lw=False, use_fwlw=False):
+    def __init__(self, field_dims, embed_dim, use_lw=False, use_fwlw=False, use_emb_bag=False, use_qr_emb=False):
         super().__init__()
         self.num_fields = len(field_dims)
         self.use_lw = use_lw
         self.use_fwlw = use_fwlw
+        self.use_emb_bag = use_emb_bag
+        self.use_qr_emb = use_qr_emb
         self.linear = FeaturesLinear(field_dims)
         self.fwfm_linear = torch.nn.Linear(embed_dim, self.num_fields, bias=False)
-        self.fwfm = FieldWeightedFactorizationMachine(field_dims, embed_dim)
-        #TODO shallow dropout?
+        self.fwfm = FieldWeightedFactorizationMachine(field_dims, embed_dim, use_emb_bag=use_emb_bag, use_qr_emb=use_qr_emb)
 
     def forward(self, x):
         """
         :param x: Long tensor of size ``(batch_size, num_fields)``
         """
-        x_offset = x + x.new_tensor(self.fwfm.offsets).unsqueeze(0)
-        embed_x = [self.fwfm.embeddings[i](x_offset[:, i]) for i in range(self.num_fields)]
-
-        fwfm_linear = torch.einsum('ijk,ik->ijk', [torch.stack(embed_x), self.fwfm_linear.weight])
-        fwfm_first_order = torch.sum(torch.einsum('ijk->ji', [fwfm_linear]), dim=1, keepdim=True)
+        if self.use_emb_bag or self.use_qr_emb:
+            embed_x = [self.fwfm.embeddings[i](torch.unsqueeze(x[:, i], 1)) for i in range(self.num_fields)]
+        else:
+            embed_x = [self.fwfm.embeddings[i](x[:, i]) for i in range(self.num_fields)] # TODO most computation here?
 
         fwfm_second_order = torch.sum(self.fwfm(torch.stack(embed_x)), dim=1, keepdim=True)
 
         if self.use_lw:
             x = self.linear(x) + fwfm_second_order
         elif self.use_fwlw:
+            fwfm_linear = torch.einsum('ijk,ik->ijk', [torch.stack(embed_x), self.fwfm_linear.weight])
+            fwfm_first_order = torch.sum(torch.einsum('ijk->ji', [fwfm_linear]), dim=1, keepdim=True)
             x = fwfm_first_order + fwfm_second_order
         else:
             x = fwfm_second_order
 
         return torch.sigmoid(x.squeeze(1))
+
 
 class FactorizationMachine(torch.nn.Module):
 
@@ -195,6 +223,7 @@ class FactorizationMachine(torch.nn.Module):
         if self.reduce_sum:
             ix = torch.sum(ix, dim=1, keepdim=True)
         return 0.5 * ix
+
 
 class FactorizationMachineModel(torch.nn.Module):
     """
