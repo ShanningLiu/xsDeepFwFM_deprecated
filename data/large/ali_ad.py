24d654c6309008f4c03816d17cf98b0ae8180e74
ShanningLiu <93221590+ShanningLiu@users.noreply.github.com>
2022-04-02 16:12:32 -0400

    git ignore works

---
+++ data/large/ali_ad.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import warnings
+
+from sklearn.preprocessing import MinMaxScaler
+
+warnings.simplefilter('ignore')
+
+def mem_usage(pandas_obj):
+    if isinstance(pandas_obj,pd.DataFrame):
+        usage_b = pandas_obj.memory_usage(deep=True).sum()
+    else: # we assume if not a df it's a series
+        usage_b = pandas_obj.memory_usage(deep=True)
+    usage_mb = usage_b / 1024 ** 2 # convert bytes to megabytes
+    return "{:03.2f} MB".format(usage_mb)
+
+raw_sample_df = pd.read_csv('../../archive/raw_sample.csv')
+ad_feature_df = pd.read_csv('../../archive/ad_feature.csv')
+user_profile_df=pd.read_csv('../../archive/user_profile.csv')
+
+test_size_mb = raw_sample_df.memory_usage().sum() / 1024 / 1024
+test_size_mb1 = ad_feature_df.memory_usage().sum() / 1024 / 1024
+test_size_mb2 = user_profile_df.memory_usage().sum() / 1024 / 1024
+print("raw_sample_df memory size: %.2f MB" % test_size_mb)
+print("ad_feature_df memory size: %.2f MB" % test_size_mb1)
+print("user_profile_df memory size: %.2f MB" % test_size_mb2)
+
+raw_sample_df.info(memory_usage='deep')
+
+optimized_gl = raw_sample_df.copy()
+
+gl_int = raw_sample_df.select_dtypes(include=['int'])
+converted_int = gl_int.apply(pd.to_numeric,downcast='unsigned')
+optimized_gl[converted_int.columns] = converted_int
+
+
+gl_obj = raw_sample_df.select_dtypes(include=['object']).copy()
+converted_obj = pd.DataFrame()
+for col in gl_obj.columns:
+    num_unique_values = len(gl_obj[col].unique())
+    num_total_values = len(gl_obj[col])
+    if num_unique_values / num_total_values < 0.5:
+        converted_obj.loc[:,col] = gl_obj[col].astype('category')
+    else:
+        converted_obj.loc[:,col] = gl_obj[col]
+optimized_gl[converted_obj.columns] = converted_obj
+print("Original Ad Feature dataframe:{0}".format(mem_usage(raw_sample_df)))
+print("Memory Optimised Ad Feature dataframe:{0}".format(mem_usage(optimized_gl)))
+
+raw_sample_df = optimized_gl.copy()
+raw_sample_df_new = raw_sample_df.rename(columns = {"user": "userid"})
+
+ad_feature_df.info(memory_usage='deep')
+
+optimized_g2 = ad_feature_df.copy()
+
+g2_int = ad_feature_df.select_dtypes(include=['int'])
+converted_int = g2_int.apply(pd.to_numeric,downcast='unsigned')
+optimized_g2[converted_int.columns] = converted_int
+
+g2_float = ad_feature_df.select_dtypes(include=['float'])
+converted_float = g2_float.apply(pd.to_numeric,downcast='float')
+optimized_g2[converted_float.columns] = converted_float
+
+print("Original Ad Feature dataframe:{0}".format(mem_usage(ad_feature_df)))
+print("Memory Optimised Ad Feature dataframe:{0}".format(mem_usage(optimized_g2)))
+
+user_profile_df.info(memory_usage='deep')
+
+optimized_g3 = user_profile_df.copy()
+
+g3_int = user_profile_df.select_dtypes(include=['int'])
+converted_int = g3_int.apply(pd.to_numeric,downcast='unsigned')
+optimized_g3[converted_int.columns] = converted_int
+
+g3_float = user_profile_df.select_dtypes(include=['float'])
+converted_float = g3_float.apply(pd.to_numeric,downcast='float')
+optimized_g3[converted_float.columns] = converted_float
+
+print("Original User Feature dataframe:{0}".format(mem_usage(user_profile_df)))
+print("Memory Optimised User Feature dataframe:{0}".format(mem_usage(optimized_g3)))
+
+df1 = raw_sample_df_new.merge(optimized_g3, on="userid")
+final_df = df1.merge(optimized_g2, on="adgroup_id")
+final_df.head()
+
+final_df['hist_cate_id'] = final_df['cate_id']
+final_df['hist_adgroup_id'] = final_df['adgroup_id']
+
+first_column = final_df.pop('clk')
+final_df.insert(0, 'clk', first_column)
+
+# = categorical features
+sparse_features = ['userid', 'adgroup_id', 'final_gender_code', 'cate_id']
+
+# = numerical features
+dense_features = ['price']
+
+sequence_features = ['hist_cate_id', 'hist_adgroup_id']
+
+behavior_feature_list = [feat for feat in final_df.columns if feat in ['adgroup_id', 'cate_id']]
+
+final_df[sparse_features] = final_df[sparse_features].fillna('-1', )
+final_df[sequence_features] = final_df[sequence_features].fillna('-1', )
+final_df[dense_features] = final_df[dense_features].fillna(0, )
+target = ['clk']
+
+mms = MinMaxScaler(feature_range=(0, 1))
+final_df[dense_features] = mms.fit_transform(final_df[dense_features])
+
+for col in sparse_features:
+    tmp = final_df.pop(col)
+    final_df.insert(20, col, tmp)
+
+print(final_df)
+
+final_df.to_csv('ali_click.csv', header=None, index=None)
---
+++ data/large/preprocess_ali.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+ali_click = pd.read_csv('ali_click.csv', header=None, index_col=None, low_memory=False)
+
+
+def cnt_freq_train(inputs):
+    count_freq = []
+    for col in inputs:
+        count_freq.append(inputs[col].value_counts())
+
+    return count_freq
+
+
+def generate_feature_map_and_train_csv(inputs, file_feature_map):
+    feature_map = []
+    for freq in freq_dict:
+        col_map = {}
+        for idx, (key, value) in enumerate(freq.items()):
+            col_map[key] = idx + 1
+
+        feature_map.append(col_map)
+
+
+
+    # inputs.fillna(0, inplace=True)
+
+    # write feature_map file
+    f_map = open(file_feature_map, 'w')
+    for i in range(17, len(list(inputs.columns))):
+        for feature in feature_map[i]:
+            if feature_map[i][feature] != 0:
+                f_map.write(str(i) + ',' + str(feature) + ',' + str(feature_map[i][feature]) + '\n')
+    return feature_map
+
+
+def generate_valid_csv(inputs, valid_csv, feature_map):
+    fout = open(valid_csv, 'w')
+    for idx, line in enumerate(inputs):
+        line = line.replace('\n', '').split('\t')
+        output_line = [line[0]]
+        for i in range(1, 21):
+            if i < 14:
+                output_line.append(line[i])
+            elif line[i] in feature_map[i]:
+                output_line.append(feature_map[i][line[i]])
+            else:
+                output_line.append('0')
+        output_line = ','.join(output_line)
+        fout.write(output_line + '\n')
+
+# no test data with labels online available
+print('Split the orignal dataset into train and valid dataset.')
+train_raw = ali_click.sample(frac=0.9, random_state=0, axis=0)
+test = ali_click[~ali_click.index.isin(train_raw.index)]
+
+valid = train_raw.sample(frac=0.5, random_state=0, axis=0)
+train = train_raw[~train_raw.index.isin(valid.index)]
+
+train.fillna(0, inplace=True)
+valid.fillna(0, inplace=True)
+test.fillna(0, inplace=True)
+#
+
+#
+# valid = pd.read_csv('ali_valid.csv', header=None, index_col=None, low_memory=False)
+# train = pd.read_csv('ali_train.csv', header=None, index_col=None, low_memory=False)
+# test = pd.read_csv('ali_test.csv', header=None, index_col=None, low_memory=False)
+
+#
+# # Not the best way, follow xdeepfm
+print('Count the frequency.')
+freq_dict = cnt_freq_train(train)
+
+#
+# print('Generate the feature map and impute the training dataset.')
+feature_map = generate_feature_map_and_train_csv(train, 'ali_feature_map')
+#
+# print('Impute the valid dataset.')
+# generate_valid_csv(valid, 'ali_valid.csv', feature_map)
+# generate_valid_csv(test, 'ali_test.csv', feature_map)
+valid.to_csv('ali_valid.csv', header=None, index=None)
+train.to_csv('ali_train.csv', header=None, index=None)
+test.to_csv('ali_test.csv', header=None, index=None)
